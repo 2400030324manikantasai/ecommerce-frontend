c1025b61650ae00cc4dd9dd9a7a309f9b9029b60
2400030324manikantasai <2400030324@kluniversity.in>
2026-03-25 23:39:59 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ecommerce-backend-production-d64c.up.railway.app/api"
+  baseURL: "https://ecommerce-backend-production-0826.up.railway.app"
 });
 
 export default API;
